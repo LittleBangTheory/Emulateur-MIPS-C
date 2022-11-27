ec67df465963d2cb1ad8a3fd386b8133d51cf187
LittleBangTheory <gabicho05@gmail.com>
2022-11-27 21:46:03 +0100

    update system.s

--- tests/system.s
+++ tests/system.s
@@ -1,13 +1,14 @@
-J 
-JAL 
-JR 
+J 2
+ADDI $2, $0, 3
+JAL 2  
+JR $2
 NOP
 LUI $2, 4
-LW 
+LW $2, 2($2)
 ROTR $3, $2, 4 
 SLL $4, $3, 5
 SRL $5, $4, 2
-SW 
+SW $5, 2($2)
 SYSCALL
 
 # EXPECTED_ASSEMBLY
